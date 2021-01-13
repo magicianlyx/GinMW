bcefff6f23a3a59d2a24334e8380b3f719159934
magicianlyx <maglyx@qq.com>
2021-01-13 18:02:17 +0800

    添加http报文打印插件

---
+++ mw/http_log/logger.go
@@ -0,0 +1,75 @@
+package http_log
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var space = []byte(" ")
+var enter = []byte("\r")
+var feed = []byte("\n")
+
+func Log(log *log.Logger) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.Writer = initResponseWrite(c.Writer)
+		RequestLog(log, c)
+		c.Next()
+		ResponseLog(log, c)
+	}
+
+}
+
+func RequestLog(log *log.Logger, c *gin.Context) {
+	method := c.Request.Method
+	url := c.Request.URL.String()
+	proto := c.Request.Proto
+	header := c.Request.Header
+	body, _ := c.GetRawData()
+
+	data := mergeBytes(
+		[]byte(method), space, []byte(url), space, []byte(proto), enter, feed,
+		mergeHeader(header), feed,
+		body,
+	)
+
+	log.Writer().Write(data)
+}
+
+func ResponseLog(log *log.Logger, c *gin.Context) {
+	rw := c.Writer.(*responseWriter)
+
+	proto := c.Request.Proto
+	status := strconv.Itoa(rw.Status())
+	statusText := http.StatusText(rw.Status())
+
+	header := rw.Header()
+	body := rw.GetBody()
+
+	data := mergeBytes(
+		[]byte(proto), space, []byte(status), space, []byte(statusText), enter, feed,
+		mergeHeader(header), feed,
+		body,
+	)
+
+	log.Writer().Write(data)
+}
+
+func mergeBytes(datas ...[]byte) []byte {
+	r := make([]byte, 0)
+	for i := range datas {
+		r = append(r, datas[i]...)
+	}
+	return r
+}
+
+func mergeHeader(headers http.Header) []byte {
+	r := make([]byte, 0)
+	for k := range headers {
+		t := []byte(fmt.Sprintf("%v:%v%v%v", k, headers[k], enter, feed))
+		r = append(r, t...)
+	}
+	return r
+}
---
+++ mw/http_log/response_writer.go
@@ -0,0 +1,36 @@
+package http_log
+
+import "github.com/gin-gonic/gin"
+
+type responseWriter struct {
+	body []byte
+	gin.ResponseWriter
+}
+
+func initResponseWrite(rw gin.ResponseWriter) *responseWriter {
+	return &responseWriter{[]byte{}, rw}
+}
+
+func (w *responseWriter) Write(data []byte) (int, error) {
+	w.body = make([]byte, len(data))
+	copy(w.body, data)
+	return w.ResponseWriter.Write(data)
+}
+
+func (w *responseWriter) WriteString(s string) (int, error) {
+	w.body = make([]byte, len([]byte(s)))
+	copy(w.body, []byte(s))
+	return w.ResponseWriter.WriteString(s)
+}
+
+func (w *responseWriter) Status() int {
+	return w.ResponseWriter.Status()
+}
+
+func (w *responseWriter) Written() bool {
+	return w.ResponseWriter.Written()
+}
+
+func (w *responseWriter) GetBody() []byte {
+	return w.body
+}
